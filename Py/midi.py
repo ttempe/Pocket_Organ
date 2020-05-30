c4536edf8acf571ace6a793bfb2411d4df04a730
Thomas TEMPÉ <thomas.tempe@alysse.org>
2020-05-30 23:04:15 +0800

    Cleanup & robustification
    Move instrument names to a separate file, add data range checks to avoid hard-to-debug issues.

--- Py/midi.py
+++ Py/midi.py
@@ -16,21 +16,27 @@ class Midi:
         
 
     def note_on(self, channel, note, vel=64):
-        self.buf3[0] = 0x90 | channel
-        self.buf3[1] = note
-        self.buf3[2] = vel 
-        self.uart.write(self.buf3)
+         self.buf3[0] = 0x90 | (channel & 0x0F)
+         self.buf3[1] = note & 0x7F
+         self.buf3[2] = vel & 0x7F
+         self.uart.write(self.buf3)
         
     def note_off(self, channel, note, vel=64):
-        self.buf3[0] = 0x80 | channel
-        self.buf3[1] = note
-        self.buf3[2] = vel
+        self.buf3[0] = 0x80 | (channel & 0x0F)
+        self.buf3[1] = note & 0x7F
+        self.buf3[2] = vel & 0x7F
         self.uart.write(self.buf3)
-    
+
+    def all_off(self, channel):
+        self.buf3[0] = 0xB0 | (channel & 0x0F)
+        self.buf3[1] = 123
+        self.buf3[2] = 0
+        self.uart.write(self.buf3)
+
     def set_instr(self, channel, instr):
-        self.buf3[0] = 0xC0 | channel
+        self.buf3[0] = 0xC0 | (channel & 0x0F)
         self.buf3[1] = 0
-        self.buf3[2] = instr & 0x8F
+        self.buf3[2] = instr & 0x7F
 
     def test2(self):
         while 1 :
@@ -51,146 +57,3 @@ class Midi:
             time.sleep(1)
             self.uart.write(b"\x80\x40\x60")
 
-
-instrument_families = [
-    "Piano",
-    "Chromatic percussion",
-    "Organ",
-    "Guitar",
-    "Bass",
-    "Strings I",
-    "Strings II",
-    "Brass",
-    "Reed",
-    "Pipe",
-    "Synth lead",
-    "Synth pad",
-    "Synth effects",
-    "Ethnic",
-    "Percussive",
-    "Sound effects"
-    ]
-
-instrument_names = [
-    "Acoustic Grand Piano",
-    "Bright Acoustic Piano",
-    "Electric Grand Piano",
-    "Honky-tonk Piano",
-    "Electric Piano 1",
-    "Electric Piano 2",
-    "Harpsichord",
-    "Clavinet",
-    "Celesta",
-    "Glockenspiel",
-    "Music Box",
-    "Vibraphone",
-    "Marimba",
-    "Xylophone",
-    "Tubular Bells",
-    "Dulcimer",
-    "Drawbar Organ",
-    "Percussive Organ",
-    "Rock Organ",
-    "Church Organ",
-    "Reed Organ",
-    "Accordion",
-    "Harmonica",
-    "Tango Accordion",
-    "Acoustic Guitar (nylon)",
-    "Acoustic Guitar (steel)",
-    "Electric Guitar (jazz)",
-    "Electric Guitar (clean)",
-    "Electric Guitar (muted)",
-    "Overdriven Guitar",
-    "Distortion Guitar",
-    "Guitar harmonics",
-    "Acoustic Bass",
-    "Electric Bass (finger)",
-    "Electric Bass (pick)",
-    "Fretless Bass",
-    "Slap Bass 1",
-    "Slap Bass 2",
-    "Synth Bass 1",
-    "Synth Bass 2",
-    "Violin",
-    "Viola",
-    "Cello",
-    "Contrabass",
-    "Tremolo Strings",
-    "Pizzicato Strings",
-    "Orchestral Harp",
-    "Timpani",
-    "String Ensemble 1",
-    "String Ensemble 2",
-    "Synth Strings 1",
-    "Synth Strings 2",
-    "Choir Aahs",
-    "Voice Oohs",
-    "Synth Voice",
-    "Orchestra Hit",
-    "Trumpet",
-    "Trombone",
-    "Tuba",
-    "Muted Trumpet",
-    "French Horn",
-    "Brass Section",
-    "Synth Brass 1",
-    "Synth Brass 2",
-    "Soprano Sax",
-    "Alto Sax",
-    "Tenor Sax",
-    "Baritone Sax",
-    "Oboe",
-    "English Horn",
-    "Bassoon",
-    "Clarinet",
-    "Piccolo",
-    "Flute",
-    "Recorder",
-    "Pan Flute",
-    "Blown Bottle",
-    "Shakuhachi",
-    "Whistle",
-    "Ocarina",
-    "Lead 1 (square)",
-    "Lead 2 (sawtooth)",
-    "Lead 3 (calliope)",
-    "Lead 4 (chiff)",
-    "Lead 5 (charang)",
-    "Lead 6 (voice)",
-    "Lead 7 (fifths)",
-    "Lead 8 (bass + lead)",
-    "Pad 1 (new age)",
-    "Pad 2 (warm)",
-    "Pad 3 (polysynth)",
-    "Pad 4 (choir)",
-    "Pad 5 (bowed)",
-    "Pad 6 (metallic)",
-    "Pad 7 (halo)",
-    "Pad 8 (sweep)",
-    "FX 1 (rain)",
-    "FX 2 (soundtrack)",
-    "FX 3 (crystal)",
-    "FX 4 (atmosphere)",
-    "FX 5 (brightness)",
-    "FX 6 (goblins)",
-    "FX 7 (echoes)",
-    "FX 8 (sci-fi)",
-    "Sitar",
-    "Banjo",
-    "Shamisen",
-    "Koto",
-    "Kalimba",
-    "Bag pipe",
-    "Fiddle",
-    "Shanai",
-    "Tinkle Bell",
-    "Agogo",
-    "Steel Drums",
-    "Woodblock",
-    "Taiko Drum",
-    "Melodic Tom",
-    "Synth Drum",
-    "Reverse Cymbal",
-    "Guitar Fr"
-    ]
