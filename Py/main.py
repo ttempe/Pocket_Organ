81d2f887c196710d410089dd8058c134d4cff397
Thomas TEMPÉ <thomas.tempe@alysse.org>
2020-07-05 20:45:11 +0800

    For debugging convenience

--- Py/main.py
+++ Py/main.py
@@ -1,3 +1,8 @@
-import pocket_organ
+from machine import Pin
+
+pin = Pin("B0", Pin.IN, Pin.PULL_UP)
+if not(pin()):
+    #Start if the "volume" key is pressed on startup
+    import pocket_organ
 
 #End
