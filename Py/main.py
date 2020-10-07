ee3309b3f1742dccd944a0b5f6596d317d7f7cbf
Thomas TEMPÉ <thomas.tempe@alysse.org>
2020-10-07 21:53:16 +0800

    doc

--- Py/main.py
+++ Py/main.py
@@ -4,5 +4,7 @@ pin = Pin("B0", Pin.IN, Pin.PULL_UP)
 if not(pin()):
     #Start if the "volume" key is pressed on startup
     import pocket_organ
+else:
+    print("Hold the Volume key while powering on to start the instrument")
 
 #End
