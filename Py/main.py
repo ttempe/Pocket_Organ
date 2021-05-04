323cfde40865ae371b3d5d46ecca70dc31d221a2
Thomas TEMPÉ <thomas.tempe@alysse.org>
2021-05-04 14:50:07 +0800

    Freeze all the code!
    Load from filesystem first, from frozen code next.
    
    Also updated startup sequence to account for capacitive buttons

--- Py/main.py
+++ Py/main.py
@@ -1,10 +1,19 @@
-import board
 
-pin = board.main_startup_pin
-if not(pin()):
-    #Start if the "volume" key is pressed on startup
+#First, set Micropython to load modules from the flash by default, and fall back to frozen modules.
+from sys import path
+path.pop(0)
+path.append("")
+
+#Next, start Pocket_Organ if the "Volume" key is pressed down
+from board import main_startup_pin, keyboard_melody_led
+from time import sleep
+
+sleep(1)
+if not(main_startup_pin()):
+    print("Starting Pocket Organ")
+    keyboard_melody_led(1)
     import pocket_organ
 else:
-    print("Hold the Volume key while powering on to start the instrument")
+    print("Starting in REPL mode.\nHold the Volume key while powering on to start the instrument")
 
 #End
