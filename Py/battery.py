e282bb0cd609ba58d78b13e20314cdf692c62478
Thomas TEMPÉ <thomas.tempe@alysse.org>
2021-08-29 21:21:49 +0800

    Still broken...

--- Py/battery.py
+++ Py/battery.py
@@ -1,7 +1,7 @@
 from board import vbat, vusb, verbose
 from time import ticks_ms
 
-bat_levels = [(4.2, "bat_100"),
+bat_levels = [(4.15, "bat_100"),
               (3.7, "bat_70"),
               (3.6, "bat_20"),
               (0,   "bat_0")]
@@ -27,15 +27,19 @@ class Battery:
         self.d.disp.framebuf.fill_rect(110, 2, lvl, 4, 1)
         self.d.disp.show_top8()
 
+
     def loop(self):
+        #TODO: Test again on V19
+        #TODO: Re-draw the "charging" icon. (Maybe without a battery, only a plug or USB logo?)
         if ticks_ms()-self.last_time > 500:
-            if vusb()>4.5:
-                icon = "bat_chrg"
+            if vusb()>4.5 and self.last_lvl != None:
+                self.d.indicator("bat_chrg", 108)
+                last_lvl = None
             else:
                 lvl = min(int(14*(vbat()-3.3)/1.0),14)
-            if lvl != self.last_lvl:
-                self.disp_bat(lvl)
-                self.last_lvl = lvl
+                if lvl != self.last_lvl:
+                    self.disp_bat(lvl)
+                    self.last_lvl = lvl
             if verbose:
                 self.d.indicator_txt("-\|/"[self.disp_update%4] + str(vbat())[0:4]+"V", 58)
                 self.disp_update += 1
