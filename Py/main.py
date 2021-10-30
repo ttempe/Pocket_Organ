3d484d46afb4aab84fb18a96f05d50ba72c374d1
Thomas TEMPÉ <thomas.tempe@alysse.org>
2021-10-30 22:07:54 +0800

    Start automatically when not plugged to USB

--- Py/main.py
+++ Py/main.py
@@ -2,12 +2,20 @@
 from board import main_startup_pin, keyboard_melody_led
 from time import sleep
 
-sleep(1)
-if not(main_startup_pin()):
+def start():
     print("Starting Pocket Organ")
-    keyboard_melody_led(1)
+    keyboard_melody_led(1)#Flash Shift led (it will be turned off later)
     import pocket_organ
+
+#Start if USB is disconnected
+#Otherwise, start if Vol is pressed
+import pyb
+p=pyb.USB_VCP()
+if p.isconnected():
+    if not(main_startup_pin()):
+        start()
 else:
-    print("Starting in REPL mode.\nHold the Volume key while powering on to start the instrument")
+    start()
+print("Not starting Pocket Organ")
 
 #End
