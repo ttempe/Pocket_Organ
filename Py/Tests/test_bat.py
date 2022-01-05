8f13ea08210e2ab095612ebe1c3139ba3719e933
Thomas TEMPÉ <thomas.tempe@alysse.org>
2022-01-05 22:48:59 +0800

    More debugging info

--- Py/Tests/test_bat.py
+++ Py/Tests/test_bat.py
@@ -2,7 +2,7 @@ import display
 import backlight
 import os
 import re
-from board import vbat, vusb
+from board import vbat, vusb, vbat_ADC, vusb_ADC
 from time import ticks_ms, sleep_ms
 
 d=display.Display()
@@ -24,7 +24,7 @@ def logfile_name():
 def loop():
     fn = logfile_name()
     fd=open(fn, "w")
-    fd.write("timestamp,time,vbat,vusb\n")
+    fd.write("timestamp,time,vbat,vusb,vbat_read16,vusb_read16,vref\n")
     fd.close()
     while(True):
         t = ticks_ms()//1000
@@ -34,7 +34,9 @@ def loop():
         d.text("Vbat: {:1.3} V".format(vbat()),1)
         d.text("Vusb: {:1.3} V".format(vusb()),2)
         fd=open(fn, "a")
-        fd.write(",".join([str(t),h,str(vbat()),str(vusb())])+"\n")
+        fd.write(",".join([str(t),h,str(vbat()),str(vusb()),str(vbat_ADC.read_u16()),str(vusb_ADC.read_u16()),str(vbat_ADC.VREF)])+"\n")
         fd.close()
         sleep_ms(10000)
+
 loop()
+#End
